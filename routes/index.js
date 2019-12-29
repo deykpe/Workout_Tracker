ecccd5315ea85b3ebae8c34dae255ba55474b99c
David Pang <dpang2@uci.edu>
2019-12-29 11:12:54 -0800

    added router.get

--- routes/index.js
+++ routes/index.js
@@ -1,2 +1,20 @@
 const db = require ('./models/workout1.js')
 const router = require ('express').Router()
+
+router.get('/api/alltransaction', (request, response){
+    db.find(({})).then(function(records){
+        response.json(records)
+    })
+    .catch((error){
+        response.send(error)
+    })
+})
+
+router.get('/api/alltransaction', (request, response){
+    db.find(({})).then(function(records){
+        response.json(records)
+    })
+    .catch((error){
+        response.send(error)
+    })
+})
