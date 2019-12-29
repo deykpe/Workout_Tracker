c3f64b6dd5408ec07e1d2fffbfdfe10ff0a8f470
David Pang <dpang2@uci.edu>
2019-12-29 11:05:43 -0800

    added mongoose schema

---
+++ models/workout1.js
@@ -0,0 +1,26 @@
+const mongoose = require ('mongoose');
+const Schema = mongoose.Schema
+
+const workout1schema = new Schema(
+    {
+        workoutname: (
+            type: String,
+            required: "enter workoutname"
+            
+        ),
+        numberofreps: (
+            type: Number,
+            required: "enter rep count"
+            
+        ),
+        date: (
+            type: Date,
+            default: Date.now
+            
+        )
+    }
+)
+
+const Workout1 = mongoose.model('Workout1', workout1schema)
+
+module.exports = Workout1
