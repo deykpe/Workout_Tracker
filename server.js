d4e24723cf8c0078a52692969d24e34ce052c338
deykpe <dpang2@uci.edu>
2020-01-15 18:21:21 -0800

    strings

--- server.js
+++ server.js
@@ -1,24 +1,25 @@
-var express = require ('express');
-var mongoose = require ('mongoose');
+var express = require('express');
+var mongoose = require('mongoose');
+var MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost/workout';
+const PORT = process.env.PORT || 8080;
 
-var port = 8080
+// const db = require("./models");
 
 var app = express();
 
-app.use(express.urlencoded({ extended: true }))
+app.use(express.urlencoded({ extended: true }));
 
-app.use(express.json())
-mongoose.connect('mongodb://localhost/workout', {
-    useNewUrlParser:true, 
-    useFindAndModify:false
-})
+app.use(express.json());
 
-app.use(require('./routes'))
-app.use(express.static('public'))
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workout', {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useFindAndModify: false
+});
+mongoose.connect(MONGODB_URI);
+app.use(require('./routes'));
+app.use(express.static('public'));
 
-app.listen(port, function() {
-console.log('port is working on 8080')
-})
-
-
-
+app.listen(PORT, function() {
+  console.log('port is working on 8080');
+});
